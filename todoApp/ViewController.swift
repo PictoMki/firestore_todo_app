4346c191cb752acffd7aafc9143b0e78f26abc5f
tomoki inoue <>
2020-10-25 20:38:49 +0900

    新規登録・ログインの処理を修正

--- todoApp/ViewController.swift
+++ todoApp/ViewController.swift
@@ -17,32 +17,33 @@ class ViewController: UIViewController {
         if let email = registerEmailTextField.text,
             let password = registerPasswordTextField.text,
             let name = registerNameTextField.text {
+            // ①FirebaseAuthにemailとpasswordでアカウントを作成する
             Auth.auth().createUser(withEmail: email, password: password, completion: { (result, error) in
                 if let user = result?.user {
-                    // 成功の場合
                     print("ユーザー作成完了 uid:" + user.uid)
-                    Firestore.firestore().collection("users").document().setData([
+                    // ②FirestoreのUsersコレクションにdocumentID = ログインしたuidでemailとnameを作成する
+                    Firestore.firestore().collection("users").document(user.uid).setData([
                         "email": email,
                         "name": name
                     ], completion: { error in
                         if let error = error {
-                            // 失敗した場合
+                            // ②が失敗した場合
                             print("Firestore 新規登録失敗 " + error.localizedDescription)
-                            let dialog = UIAlertController(title: "Firestore 新規登録失敗", message: error.localizedDescription, preferredStyle: .alert)
+                            let dialog = UIAlertController(title: "新規登録失敗", message: error.localizedDescription, preferredStyle: .alert)
                             dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                             self.present(dialog, animated: true, completion: nil)
                         } else {
-                            // 成功の場合
                             print("ユーザー作成完了 name:" + name)
+                            // ③成功した場合はTodo一覧画面に画面遷移を行う
                             let storyboard: UIStoryboard = self.storyboard!
                             let next = storyboard.instantiateViewController(withIdentifier: "TodoListViewController")
                             self.present(next, animated: true, completion: nil)
                         }
                     })
                 } else if let error = error {
-                    // 失敗した場合
+                    // ①が失敗した場合
                     print("Firebase Auth 新規登録失敗 " + error.localizedDescription)
-                    let dialog = UIAlertController(title: "Firebase Auth 新規登録失敗", message: error.localizedDescription, preferredStyle: .alert)
+                    let dialog = UIAlertController(title: "新規登録失敗", message: error.localizedDescription, preferredStyle: .alert)
                     dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                     self.present(dialog, animated: true, completion: nil)
                 }
